68a470a0e3f3c5444ab2cff104e3e4cc9d9cafdf
sherif <hzaimiacherif@gmail.com>
2024-11-04 23:35:46 +0100

    Enable CORS for all origins

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const port = process.env.PORT || 3000;
 
 // تمكين CORS فقط للطلبات القادمة من https://app.inno-acc.com
 app.use(cors({
-  origin: 'https://app.inno-acc.com'
+ origin: ['https://app.inno-acc.com',
+   'chrome-extension://imhiiignfblghjjhpjfpgedinddaobjf']
 }));
 
 // الاتصال بقاعدة بيانات MongoDB Atlas
